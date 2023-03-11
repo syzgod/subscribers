f9c13c545e6dd28683a2cfafd1bcca6615707c8c
syzgod <viktor.berczeli@gmail.com>
2023-03-12 00:01:19 +0100

    Setup basic layout

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Grid } from '@mui/material';
+
+const ProfileCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Card elevation={4}>Subscriber</Card>
+    </Grid>
+  );
+};
+
+export default ProfileCard;
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,12 +19,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
