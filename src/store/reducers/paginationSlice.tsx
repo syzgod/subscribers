0a56230421dd24b454c9ca6f1d170a11d0656085
syzgod <viktor.berczeli@gmail.com>
2023-03-18 09:31:04 +0100

    Start pagination slice

---
+++ src/store/reducers/paginationSlice.tsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { enqueueSnackbar } from 'notistack';
+
+
+const initialState: pageState = {
+  actualPage:
+  fromPage:
+  toPage:
+};
+
+export const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    prevPage: (state: any) => {
+      state - 1
+    },
+    nextPage: (state) => {state + 1}
+  },
+});
+
+export const { toggleTheme } = themeSlice.actions;
+
+export default themeSlice.reducer;
